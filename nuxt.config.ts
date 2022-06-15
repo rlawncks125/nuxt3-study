9c567f3567fee39ad21679a0b44aa45e86978d49
김주찬 <rlawncks125@naver.com>
2022-06-15 22:50:42 +0900

    css선언

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,4 @@
+import { defineNuxtModule } from "@nuxt/kit";
 import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
@@ -6,5 +7,13 @@ export default defineNuxtConfig({
     // https://composition-api.nuxtjs.org/getting-started/setup#quick-start
     //  '@nuxtjs/composition-api/module', // nuxt2 에서 사용할경우
     "@pinia/nuxt",
+  ],
+  modules: [
+    // css 선언
+    defineNuxtModule({
+      setup(options, nuxt) {
+        nuxt.options.css.push("@/assets/test.css");
+      },
+    }),
   ],
 });
